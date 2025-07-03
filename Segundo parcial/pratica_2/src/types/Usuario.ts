571ab70b3eabcde0996299c03f34495ae668418c
Dav1dCeva <davidceva0gmai.com>
2025-07-02 21:00:44 -0500

    avance practica 2

---
+++ Segundo parcial/pratica_2/src/types/Usuario.ts
@@ -0,0 +1,14 @@
+export interface usuario{
+    id: number,
+    nombre: string,
+    estado: boolean,
+    direccion: string,
+    telefono: string,
+    email: string
+}
+export interface CreateUsuarioRequest {
+  nombre: string;
+  email: string;
+  direccion: string;
+  telefono: string; 
+} 
